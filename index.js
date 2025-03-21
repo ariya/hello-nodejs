22b7bd3554ad0380741b18d845a62e0f713be4f2
Ariya Hidayat <ariya.hidayat@gmail.com>
2025-03-20 19:13:59 -0700

    Basic HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello, Nodejs!');
+const http = require('http');
+
+const serve = async (port, hostname = '127.0.0.1') => {
+  const server = http.createServer(async (request, response) => {
+    const { url } = request;
+    if (url === '/' || url === '/index.html') {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end('<h1>Hello, Node.js!</h1>');
+    }
+  });
+
+  server.listen(port);
+  console.log('Listening on', hostname, 'port', port);
+};
+
+(async () => {
+  await serve(3000);
+})();
